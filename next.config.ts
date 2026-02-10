00f4e18b4c76d46c98c0bb26a3d89f85c6b15118
sebanimm <gks32114221@gmail.com>
2026-02-10 16:47:30 +0900

    feat: add Prettier configuration and update Next.js image settings

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ['via.placeholder.com'],
-  },
+  images: {},
   allowedDevOrigins: ['http://localhost:3000', 'http://192.168.202.207:3000'],
 };
 
